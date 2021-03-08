57bb6ba79a2a80434f7a2bdee743a1537fa4c17d
NotTheRealEdmund <nottherealedmund@gmail.com>
2021-03-08 21:29:46 +0800

    Convert TCP to UDP
    Replace SOCK_STREAM with SOCK_DGRAM, remove listen and accept calls

--- tcp_ser3.c
+++ tcp_ser3.c
@@ -5,21 +5,14 @@ tcp_ser.c: the source file of the server in tcp transmission
 
 #include "headsock.h"
 
-#define BACKLOG 10
-
 void str_ser(int sockfd);                                                        // transmitting and receiving function
 
 int main(void)
 {
-	int sockfd, con_fd, ret;
+	int sockfd;
 	struct sockaddr_in my_addr;
-	struct sockaddr_in their_addr;
-	int sin_size;
 
-//	char *buf;
-	pid_t pid;
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);          //create socket
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);          //create socket
 	if (sockfd <0)
 	{
 		printf("error in socket!");
@@ -27,41 +20,19 @@ int main(void)
 	}
 	
 	my_addr.sin_family = AF_INET;
-	my_addr.sin_port = htons(MYTCP_PORT);
+	my_addr.sin_port = htons(MYUDP_PORT);
 	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);//inet_addr("172.0.0.1");
 	bzero(&(my_addr.sin_zero), 8);
-	ret = bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr));                //bind socket
-	if (ret <0)
+	if (bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) <0)
 	{
 		printf("error in binding");
-		exit(1);
-	}
-	
-	ret = listen(sockfd, BACKLOG);                              //listen
-	if (ret <0) {
-		printf("error in listening");
 		exit(1);
 	}
 
 	while (1)
 	{
 		printf("waiting for data\n");
-		sin_size = sizeof (struct sockaddr_in);
-		con_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);            //accept the packet
-		if (con_fd <0)
-		{
-			printf("error in accept\n");
-			exit(1);
-		}
-
-		if ((pid = fork())==0)                                         // creat acception process
-		{
-			close(sockfd);
-			str_ser(con_fd);                                          //receive packet and response
-			close(con_fd);
-			exit(0);
-		}
-		else close(con_fd);                                         //parent process
+		str_ser(sockfd);
 	}
 	close(sockfd);
 	exit(0);
